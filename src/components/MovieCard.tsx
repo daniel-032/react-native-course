e36e31dfd756e46878970b05b642684255267963
daniel-032 <daniel.maldonado@assuresoft.tech>
2025-06-13 17:08:05 -0400

    rnc-7 carrousel moviees

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,60 @@
+import {useEffect} from 'react';
+import {Image, Text, View} from 'react-native';
+
+type MovieCardProps = {
+  imageUri: string;
+  title: string;
+  puntuation?: number;
+  typeTitle: 'textBelow' | 'textOver';
+};
+
+export function MovieCard({
+  imageUri,
+  title,
+  puntuation = 1,
+  typeTitle,
+}: MovieCardProps) {
+  useEffect(() => {
+    console.log(imageUri);
+  }, []);
+
+  return (
+    <View style={{alignItems: 'center', paddingHorizontal: 5}}>
+      <Image
+        source={{uri: imageUri}}
+        style={{
+          height: 200,
+          width: 150,
+          borderRadius: 15,
+        }}
+      />
+      {typeTitle === 'textBelow' ? (
+        <View style={{paddingTop: 5}}>
+          <Text
+            style={{
+              color: 'white',
+              textAlign: 'center',
+            }}>
+            {title}
+          </Text>
+        </View>
+      ) : (
+        <View style={{position: 'relative', width: 150, bottom: 40, flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+          <Text
+            style={{
+              color: 'white',
+              width: "60%"
+            }}>
+            {title}
+          </Text>
+          <Text
+            style={{
+              color: 'white',
+            }}>
+            ⭐{puntuation}
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+}
